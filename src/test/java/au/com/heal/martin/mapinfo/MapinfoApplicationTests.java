5a4f7d59ff4ff6b249c95b5aa160a223d87ce894
Martin Heal <heal.martin@gmail.com>
2025-07-29 16:34:11 +1000

    Initial project shell.

---
+++ src/test/java/au/com/heal/martin/mapinfo/MapinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.heal.martin.mapinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
